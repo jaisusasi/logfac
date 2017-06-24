7fab02f0e283bc20c5d9c81476644f77b607bafe
jaisusasi <jaisusasi@gmail.com>
2017-06-23 20:24:33 -0700

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 ﻿require('rootpath')();
 var express = require('express');
+var formidable = require('express-formidable');
 var app = express();
+app.use(formidable());
+
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var expressJwt = require('express-jwt');
@@ -10,6 +13,9 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//app.use(bodyParser.json({limit: '50mb'}));
+//app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+//app.use(expressformidable)
 // use JWT auth to secure the api, the token can be passed in the authorization header or querystring
 /*app.use(expressJwt({
     secret: config.secret,
@@ -23,10 +29,15 @@ app.use(bodyParser.json());
     }
 }).unless({ path: ['/users/authenticate', '/users/register'] }));*/
 
+
+
 // routes
 app.use('/users', require('./controllers/users.controller'));
 app.use('/macaddr', require('./controllers/macaddr.controller'));
 app.use('/snumbers', require('./controllers/snum.controller'));
+app.use('/firmware', require('./controllers/firmware.controller'));
+app.use('/board', require('./controllers/board.controller'));
+app.use('/macaddr_serialnumber', require('./controllers/macAddr_sNum.controller'));
 
 // start server
 var port = process.env.NODE_ENV === 'production' ? 80 : 4000;
